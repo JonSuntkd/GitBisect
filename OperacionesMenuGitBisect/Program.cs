fdfe79c31ba97113ead79b321ef90a9626263e0e
Dayana Castillo <daycasti@pichincha.com>
2023-02-09 16:42:25 -0500

    third commit  impar

--- OperacionesMenuGitBisect/Program.cs
+++ OperacionesMenuGitBisect/Program.cs
@@ -8,7 +8,7 @@ do
     Console.WriteLine("\n\n --------ELIGE LA OPCION------\n");
     Console.WriteLine("1.Factorial");
     Console.WriteLine("2.Numeros pares");
-
+    Console.Write("3.Numeros impares");
     Console.WriteLine("3.Salir");
     Console.Write("\n Elija la opcion que desee:  ");
     opcion = Convert.ToInt32(Console.ReadLine());
@@ -48,6 +48,25 @@ do
             Console.ReadKey();
             break;
         case 3:
+            Console.Clear();
+            Console.WriteLine("\n\t\t Numeros impares ");
+            do
+            {
+                Console.Write("Por favor Digite un número entero positivo: ");
+                num = int.Parse(Console.ReadLine());
+
+            } while (num < 0);
+            for (char i = 0; i < num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write("\n\t Numero par: " + i);
+                }
+            }
+            Console.WriteLine("\n\n\t Presione cualquier tecla para volver al menu");
+            Console.ReadKey();
+         
+        case 4:
             Console.Clear();
             Console.WriteLine("Salir de la aplicacion");
             salir = true;
